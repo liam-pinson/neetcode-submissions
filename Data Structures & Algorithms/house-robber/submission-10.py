a80f61c903377498e2d932e9897d9a47d677ef35
liam-pinson <156864891+liam-pinson@users.noreply.github.com>
2026-05-27 09:49:13 -0700

    Add: house-robber - submission-10

---
+++ Data Structures & Algorithms/house-robber/submission-10.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        memo = [-1] * len(nums)
+        
+        def dfs(i):
+            if i >= len(nums):
+                return 0
+            if memo[i] != -1:
+                return memo[i]
+
+            res = max(dfs(i + 1), nums[i] + dfs(i + 2))
+            memo[i] = res
+            return memo[i]
+
+        return max(dfs(0), dfs(1))
